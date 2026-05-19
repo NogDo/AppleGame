24c40e0398182fd01c6fbee77fe2e81c602b2429
DESKTOP-8SBUVPF\tmdqj <ssb9292@gmail.com>
2026-05-19 23:33:34 +0900

    feat(CAppleCreator) : 사과 생성될 영역의 Anchor를 SafeArea로 맞추는 기능 추가

---
+++ Assets/02.Scripts/UI/Apple/CAppleCreator.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CAppleCreator : MonoBehaviour
+{
+    #region private 변수
+
+    private RectTransform _rt;
+
+    #endregion
+
+    private void Awake() 
+    {
+        _rt = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        Rect safeArea = Screen.safeArea;
+        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+
+        Vector2 anchorMin = safeArea.position / screenSize;
+        Vector2 anchorMax = (safeArea.position + safeArea.size) / screenSize;
+
+        _rt.anchorMin = anchorMin;
+        _rt.anchorMax = anchorMax;
+        _rt.offsetMin = Vector2.zero;
+        _rt.offsetMax = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
