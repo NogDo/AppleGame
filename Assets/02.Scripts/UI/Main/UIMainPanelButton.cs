859d718644cde98c30f97dfd6dd5299f79902f7e
DESKTOP-N4OVVNK\신승범 <ssb9292@naver.com>
2026-06-17 14:03:44 +0900

    refactor : 패널들 활성화 상태를 일괄적으로 관리 - Dynamic 캔버스의 패널 활성화 상태만 바꾸던 것을 전체 캔버스 패널의 활성화 상태를 바꾸도록 수정 - MainPanelButton이 자체적으로 관리하던 활성화 상태를 UIPanelControl로 위임

--- Assets/02.Scripts/UI/Main/UIMainPanelButton.cs
+++ Assets/02.Scripts/UI/Main/UIMainPanelButton.cs
@@ -12,23 +12,5 @@ public class UIMainPanelButton : MonoBehaviour
     private void Start()
     {
         btnStart.onClick.AddListener(() => GameManager.Instance.GameStart());
-
-        GameManager.Instance.OnGameStart += InActiveMainPanel;
-    }
-
-    private void OnDestroy()
-    {
-        if (GameManager.Instance != null)
-        {
-            GameManager.Instance.OnGameStart -= InActiveMainPanel;
-        }
-    }
-
-    /// <summary>
-    /// 메인 패널 비활성화
-    /// </summary>
-    private void InActiveMainPanel()
-    {
-        gameObject.SetActive(false);
     }
 }
--- Assets/02.Scripts/UI/Panel/UIPanelControl.cs
+++ Assets/02.Scripts/UI/Panel/UIPanelControl.cs
@@ -4,8 +4,8 @@ public class UIPanelControl : MonoBehaviour
 {
     #region private 변수
 
-    [SerializeField] private GameObject oMainPanel;
-    [SerializeField] private GameObject oGamePanel;
+    [SerializeField] private GameObject[] oArrMainPanel;
+    [SerializeField] private GameObject[] oArrGamePanel;
 
     #endregion
 
@@ -30,8 +30,15 @@ public class UIPanelControl : MonoBehaviour
     /// </summary>
     private void OnGameStart()
     {
-        oMainPanel.SetActive(false);
-        oGamePanel.SetActive(true);
+        for (int i = 0; i < oArrMainPanel.Length;i ++)
+        {
+            oArrMainPanel[i].SetActive(false);
+        }
+
+        for (int i = 0; i < oArrGamePanel.Length; i++)
+        {
+            oArrGamePanel[i].SetActive(true);
+        }
     }
 
     /// <summary>
